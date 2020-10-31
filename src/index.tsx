05b340bda6d92195d5c12b2694c8ec638337b1fd
svetlanagorchanina <gsvetlana.gorchanina@gmail.com>
2020-10-31 19:53:15 +0300

    feat: Integrate get products request

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.css";
+import App from "components/App/App";
+import { store } from "store/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import axios from 'axios';
+import axios from "axios";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 axios.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  function(error) {
+  function (error) {
     if (error.response.status === 400) {
       alert(error.response.data?.data);
     }
@@ -20,14 +21,24 @@ axios.interceptors.response.use(
   }
 );
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#378d0e",
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline/>
-      <App/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
